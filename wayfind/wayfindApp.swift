b21dde894962889c9af14c29ce14eb04e16ae599
Ashish Jamnbhulkar <asj409@nyu.edu>
2026-04-02 16:48:14 -0400

    Initial Commit

---
+++ wayfind/wayfindApp.swift
@@ -0,0 +1,17 @@
+//
+//  wayfindApp.swift
+//  wayfind
+//
+//  Created by Ashish Jambhulkar on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct wayfindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
